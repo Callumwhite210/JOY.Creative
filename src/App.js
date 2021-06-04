a299021bce47c670a429bbacbdee025d3095ce46
Callum White <callumwhite210@gmail.com>
2021-06-04 14:56:19 +0930

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <h2> JOY. Creative </h2>
-      </header>
+
     </div>
   );
 }
