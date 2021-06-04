d98b82d3f257ce2d2f9598b0fefcc2350a604dfd
Callum White <callumwhite210@gmail.com>
2021-06-04 21:59:57 +0930

    Navbar shell

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -8,13 +8,13 @@ function NavigationBar() {
     <Navbar.Brand href="#home">JOY.Creative</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
+      <Nav className="ml-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Services</Nav.Link>
         <Nav.Link href="#link">About</Nav.Link>
         <Nav.Link href="#link">Blog</Nav.Link>
         <Nav.Link href="#link">Contact</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown title="Socials" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Facebook</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Instagram</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">LinkedIn</NavDropdown.Item>
